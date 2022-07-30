c01e6164782ab898faeb2f66c9dae30f47367048
mikexiangwang <mikexiangwang@gmail.com>
2022-07-30 18:47:39 +0100

    reflect

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+const kmaxPrimeChanNum = 1000000
+const kNum = 1000000
+
+func IsPrimeSqrt(value int) (int, bool) {
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(value)))); i++ {
+		if value%i == 0 {
+			return 0, false
+		}
+	}
+	return value, value > 1
+}
+
+// write data to inchan
+func putNum(intChan chan<- int) {
+
+	for i := 2; i < kNum; i++ {
+		intChan <- i
+	}
+	close(intChan)
+	wg.Done()
+}
+
+func primeNum(intChan <-chan int, primeChan chan<- int, exitChan chan<- bool) {
+	for i := range intChan {
+		if v, ok := IsPrimeSqrt(i); ok {
+			primeChan <- v
+		} else {
+			break
+		}
+	}
+
+	wg.Done()
+
+	exitChan <- true
+}
+
+func printPrime(primeChan <-chan int) {
+	for v := range primeChan {
+		fmt.Printf("Prime number: %v\n", v)
+	}
+	wg.Done()
+}
+
+func main() {
+	mySelect()
+}
+
+func myPrime() {
+	intChan := make(chan int, 10000)
+	primeChan := make(chan int, 50000)
+	exitChan := make(chan bool, kmaxPrimeChanNum) // exit symbol for primeChan
+
+	start := time.Now().Unix()
+	wg.Add(1)
+	go putNum(intChan) // store the numbers
+	for i := 0; i < kmaxPrimeChanNum; i++ {
+		wg.Add(1)
+		go primeNum(intChan, primeChan, exitChan) // get the prime numbers
+	}
+	wg.Add(1)
+	go printPrime(primeChan) // print the numbers
+
+	// to determine if chan is full
+	wg.Add(1)
+	go func() {
+		for i := 0; i < kmaxPrimeChanNum; i++ {
+			<-exitChan
+		}
+		close(primeChan)
+		wg.Done()
+	}()
+	wg.Wait()
+	end := time.Now().Unix()
+	fmt.Printf("Finished with %v seconds\n", (end - start))
+}
+
+func mySelect() {
+	// to recover
+	defer func() { //  to catch panic
+		if err := recover(); err != nil {
+			fmt.Println("mySelect panic ", err)
+		}
+	}()
+
+	// define a chan with 10 int
+	iChan := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		iChan <- i
+	}
+
+	// define a chan with 5 strings
+	sChan := make(chan string, 5)
+	for i := 0; i < 5; i++ {
+		sChan <- "hello" + fmt.Sprintf("%d", i)
+	}
+
+	// select does not require chan to be closed
+	for {
+		select {
+		case v := <-iChan:
+			fmt.Printf("get value from iChan: %v\n", v)
+		case v := <-sChan:
+			fmt.Printf("get value from sChan %v\n", v)
+		default:
+			fmt.Printf("Finished")
+			return
+		}
+
+	}
+
+}
---
+++ myreflect/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type myInt int
+type Student struct {
+	Name  string `json:"name" form:"username"`
+	Age   int    `json:"age" form:"userage"`
+	Score int    `json:"score" form:"userscore"`
+}
+
+func (s *Student) GetInfo() string {
+	var str = fmt.Sprintf("name: %v; age: %v; score: %v", s.Name, s.Age, s.Score)
+	return str
+}
+
+func (s *Student) SetInfo(name string, age int, score int) {
+	s.Name = name
+	s.Age = age
+	s.Score = score
+}
+
+func (s *Student) Print() {
+	fmt.Println("this is print functions")
+}
+
+// print fields
+
+func PrintStudentFields(s interface{}) {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+	if t.Kind() != reflect.Struct && t.Elem().Kind() != reflect.Struct {
+		fmt.Println("this is not s struct")
+		return
+	}
+
+	//1. using field
+	f0 := t.Elem().Field(0)
+	fmt.Printf("Type: %v, Kind: %v, Name: %v, Tag1: %v, Tag2: %v\n", f0, f0.Type, f0.Name, f0.Tag.Get("json"), f0.Tag.Get("form"))
+
+	//2.  get FiledbyName
+	f1, ok := t.Elem().FieldByName("Age")
+	if ok {
+		fmt.Printf("Type: %v, Kind: %v, Name: %v, Tag1: %v, Tag2: %v\n", f1, f1.Type, f1.Name, f1.Tag.Get("json"), f1.Tag.Get("form"))
+	}
+
+	//3. from Typeof to get numfiled
+	fCnt := t.Elem().NumField()
+	fmt.Println("struct has ", fCnt, "fields")
+
+	//4. To get value from struct from Value of
+	v0 := v.Elem().FieldByName("Score")
+	fmt.Printf("Type: %v, Kind: %v\n", v0, v.Elem().Type())
+
+	for i := 0; i < fCnt; i++ {
+		f0 = t.Elem().Field(i)
+		fmt.Printf("Type: %v, Kind: %v, Name: %v, Tag1: %v, Tag2: %v\n", f0, f0.Type, f0.Name, f0.Tag.Get("json"), f0.Tag.Get("form"))
+
+	}
+
+}
+
+func PrintStudentFn(s interface{}) {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+	if t.Kind() != reflect.Struct && t.Elem().Kind() != reflect.Struct {
+		fmt.Println("this is not s struct")
+		return
+	}
+
+	//1. Method(0)
+	method0 := t.Method(0) // not the order in the file. It's according to the ASCII
+	fmt.Println(method0.Name, method0.Type)
+
+	//2. MethodByName
+	m1, ok := t.MethodByName("Print")
+	if ok {
+		fmt.Println(m1.Name, m1.Type)
+	}
+
+	// 3. exec method from value
+	v.MethodByName("Print").Call(nil)
+	str := v.MethodByName("GetInfo").Call(nil)
+	fmt.Println(str)
+
+	// 3.1 pass params to call
+	var params []reflect.Value
+	params = append(params, reflect.ValueOf("ting"))
+	params = append(params, reflect.ValueOf(23))
+	params = append(params, reflect.ValueOf(90))
+	v.MethodByName("SetInfo").Call(params)
+	info := v.MethodByName("GetInfo").Call(nil)
+	fmt.Println(info)
+
+	// 4. get NumMethod
+	fmt.Println(t.NumMethod(), v.NumMethod())
+
+}
+
+func ChangeStudentFn(s interface{}) {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+
+	if t.Kind() != reflect.Ptr {
+		fmt.Println("this is not a ptr")
+		return
+	} else if t.Kind() != reflect.Struct && t.Elem().Kind() != reflect.Struct {
+		fmt.Println("this is not a struct ptr")
+		return
+	}
+
+	// update struct filed
+	name := v.Elem().FieldByName("Name")
+	name.SetString("li")
+	age := v.Elem().FieldByName("Age")
+	age.SetInt(22)
+}
+
+func structTest() {
+	stu1 := Student{
+		Name:  "xiang",
+		Age:   30,
+		Score: 100,
+	}
+
+	//PrintStudentFields(&stu1)
+	// PrintStudentFn(&stu1)
+	ChangeStudentFn(&stu1)
+	fmt.Println(stu1)
+}
+
+func main() {
+	//reflectTypeOfTest()
+	//reflectValueOfTest()
+	structTest()
+}
+
+func reflectTypeOf(x interface{}) {
+	v := reflect.TypeOf(x)
+	fmt.Printf("Type: %v, Kind: %v, Name: %v\n", v, v.Kind(), v.Name())
+}
+
+func reflectValueOf(x interface{}) {
+
+	// user reflect to get the value the original
+
+	// vi := v.Int() + 12
+	// fmt.Println("the sum is :", vi)
+
+	// set
+
+	// v, _ := x.(*int64)
+	// *v = 120
+
+	v := reflect.ValueOf(x)
+	fmt.Printf("Value: %v, Kind: %v\n", v, v.Kind())
+	if v.Elem().Kind() == reflect.Int64 { // if x is ptr then add v.Elem().Kind()
+		v.Elem().SetInt(120)
+	} else if v.Elem().Kind() == reflect.String {
+		v.Elem().SetString("updated")
+	}
+}
+
+func reflectTypeOfTest() {
+	a := 10
+	b := 23.4
+	c := true
+	d := "hello"
+
+	reflectTypeOf(a)
+	reflectTypeOf(b)
+	reflectTypeOf(c)
+	reflectTypeOf(d)
+
+	var e myInt = 34
+	var f = Student{
+		Name: "xiang",
+		Age:  20,
+	}
+	reflectTypeOf(e)
+	reflectTypeOf(f)
+
+	var h = 25        // pointer
+	reflectTypeOf(&h) // *int
+
+	var i = [3]int{1, 2, 3} // arrage
+	reflectTypeOf(i)
+
+	var j = []int{11, 22, 33} // slice
+	reflectTypeOf(j)
+
+}
+
+func reflectValueOfTest() {
+	var a int64 = 13
+	reflectValueOf(&a)
+	fmt.Println("value of a is: ", a)
+
+	var b string = "13"
+	reflectValueOf(&b)
+	fmt.Println("value of a is: ", b)
+}
